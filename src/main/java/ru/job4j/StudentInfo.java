12242e3905283bfc270386f1447e8053ca560ac6
timurkemelbaev <timurik.88@mail.ru>
2022-03-29 21:42:01 +0500

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String [] args){
+    public static void main(String[] args) {
           System.out.println("Timur Kemelbaev");
           System.out.println("17.02.88");
     }
