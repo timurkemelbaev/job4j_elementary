6104930e082233be4597fa3f7762a137225508be
timurkemelbaev <timurik.88@mail.ru>
2022-03-29 20:54:42 +0500

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String [] args){
+          System.out.println("Timur Kemelbaev");
+          System.out.println("17.02.88");
+    }
+}
