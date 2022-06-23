63ecdcec41ab572af480d1e35be0bb1f0c2e1a12
timurkemelbaev <timurik.88@mail.ru>
2022-06-23 20:38:55 +0500

    1.2. Входные данные в тесте [#297750 #315383]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when23to57then5() {
+        double expected = 5;
+        int x1 = 2;
+        int y1 = 3;
+        int x2 = 5;
+        int y2 = 7;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when79to2219then18dot02() {
+        double expected = 18.02;
+        int x1 = 7;
+        int y1 = 9;
+        int x2 = 22;
+        int y2 = 19;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
