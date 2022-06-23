e55cb2c15cd632e9bf6d55dd2ef5de6da9dfa3b8
timurkemelbaev <timurik.88@mail.ru>
2022-06-23 21:11:27 +0500

    1.1. Что такое import [#504773 #313230]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -3,6 +3,7 @@ package ru.job4j.math;
 public class MathFunction {
 
     public static double sum(double first, double second) {
+
         return first + second;
     }
 
@@ -15,6 +16,7 @@ public class MathFunction {
     }
 
     public static double division(double first, double second) {
+
         return first / second;
     }
 }
