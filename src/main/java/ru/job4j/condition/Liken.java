d0a082ec41cb6ab6a8abf3b5ad87ff49191e0fdb
timurkemelbaev <timurik.88@mail.ru>
2022-07-03 13:06:32 +0500

    0. Операции сравнения [#235631 #317252]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class Liken {
     public static void main(String[] args) {
-        int first = 10;
-        int second = 9;
+        int first = 11;
+        int second = 10;
 
         boolean result1 = first > second;
         System.out.println(result1);
