bc56f28a9704cf1751c4f98f302f7627766a7992
timurkemelbaev <timurik.88@mail.ru>
2022-08-09 20:58:15 +0500

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayCharTest {
+
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result).isFalse();
+    }
+}
